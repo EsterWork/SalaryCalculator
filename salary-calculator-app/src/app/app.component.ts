dcd9526fe5cf2ee659853d26ba627069aac9529b
EsterWork <esti3097@gmail.com>
2025-05-18 12:05:27 +0300

    הוספת CommonModule ו-withFetch

--- salary-calculator-app/src/app/app.component.ts
+++ salary-calculator-app/src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { SalaryCalculatorComponent } from './components/salary-calculator/salary-calculator.component';
 import { SalaryResultComponent } from './components/salary-result/salary-result.component';
 import { SalaryResponse } from './models/salary-response.model';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [SalaryCalculatorComponent, SalaryResultComponent],
+  imports: [SalaryCalculatorComponent, SalaryResultComponent, CommonModule],
   templateUrl: './app.component.html'
 })
 export class AppComponent {
